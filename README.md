ea8969e067b61447a6c1b83fea0c250c107c8f96
Shalpreet Kaur <kaurshalpreet@gmail.com>
2020-08-11 03:22:57 -0700

    Initial Commit

--- README.md
+++ README.md
@@ -1,1 +1,65 @@
-# homework6-Weather-Dashboard
+# Server-Side APIs: Weather Dashboard
+
+## Description
+ This project is a weather dashboard application that will display the temperature, wind speed, humidity and UV Index of any city that you will enter in the search box. This app will run in the browser and feature dynamically updated HTML and CSS powered by a JavaScript code. The application will also display the weather forecast for next 5 days. The application will also save the search history and you can simply click on the last searched city to check it's current weather. This was something very new and a toughest one for me and I have put so much effort in learning, understanding and executing the concepts of javascript, API and API Keys. I have used the [OpenWeather API](https://openweathermap.org/api) to retrieve weather data for cities.
+ [The project can be accessed at my github repository](https://github.com/kaurshalpreet/homework4-codeQuiz)
+ ![Homework Repository Screenshot](./assets/repo.png)
+
+ ---
+
+# Table of Contents
+1. Getting Started
+1. Running the application
+1. Credits
+---
+
+## Getting Started
+
+This Project is a weather information app that will help you plan your trips accordingly. You will be able to check the temperature, wind speed, humidity and UV index for a city. You will also be able to search your last searched city to the local storage and simply clicking on it will display it's current weather and other information.
+
+![Getting Started!](./assets/gettingStarted.gif)
+
+---
+
+## Running the Application
+
+[The Code Quiz page can be accessed via github page here](https://kaurshalpreet.github.io/homework4-codeQuiz/)
+
+When you open the page, you will get a search input box to put the name of the city and click on the search icon. when you click on it, it will display the current and future conditions for that city and that city is added to the search history.
+
+![Start Quiz page](./assets/startQuiz.gif)
+
+---
+
+When you view current weather conditions for that city, You wll be able to see the city name, the date, an icon representation of weather conditions, the temperature, the humidity, the wind speed, and the UV index. You will also see the future weather condition for that day that will display the data for next 5 days, the date, an icon representation of weather conditions, the temperature, and the humidity.
+
+![Questions](./assets/questions.gif)
+
+---
+
+When you are viewing the UV index, you will see a color that indicates whether the conditions are favorable(green), moderate(yellow), or not favorable(orange) and severe(red).
+
+![Scores and initials](./assets/scoresAndInitials.gif)
+
+---
+
+When you click on a city in the search history, then you are again presented with current and future conditions for that city.
+
+![Highscore page](./assets/highscores.gif)
+
+---
+
+When you open the weather dashboard, then you are presented with the last searched city forecast.
+
+![Refresh](./assets/refresh.gif)
+
+---
+
+
+## Credits
+* UC Davis BootCamp-Instructor & TAs!
+* https://www.w3schools.com/
+* https://www.google.com/
+* https://www.youtube.com/
+* https://openweathermap.org/api
+* https://momentjs.com
---
+++ script.js
@@ -0,0 +1,175 @@
+var APIKey = "d3e71252fa567040aa1498a9d77f6b77";
+var searchButton = $(".searchButton");
+var searchInputBox = $(".searchInputBox");
+
+var cityNameEl = $(".cityName");
+var currentDateEl = $(".currentDate");
+var weatherIconEl = $(".weatherIcon");
+var searchHistoryEl = $(".searchHistory");
+
+var temperatureEl = $(".temperature");
+var humidityEl = $(".humidity");
+var windSpeedEl = $(".windSpeed");
+var uvIndexEl = $(".uvIndex");
+var cardRow = $(".card-row");
+
+var citiesArray = [];
+
+var dateDisplay = moment().format('MM/D/YYYY');
+
+searchButton.on("click", function(event){
+  event.preventDefault();
+  var cityName = searchInputBox.val();
+
+  if (cityName  === "") {
+    alert("You must enter a city");
+    return;
+  }
+
+
+  citiesArray.push(cityName);
+  searchInputBox.value = "";
+
+  storeCities();
+
+  renderCities();
+   
+  function storeCities() {
+    localStorage.setItem("searchHistory", JSON.stringify(citiesArray));
+  }
+
+  function renderCities() {
+    searchHistoryEl.empty();
+    var searchHistoryArr = JSON.parse(localStorage.getItem("searchHistory"));
+    for (var i = 0; i < citiesArray.length; i++) {
+      var cities = citiesArray[i];
+  
+      var li = $("<li>").attr("class", "historyEntry");
+      li.text(cities);
+      searchHistoryEl.prepend(li);
+
+      // searchInputBox.val( citiesArray[index] );
+    }
+  }
+//   if (JSON.parse(localStorage.getItem("searchHistory")) === null) {
+//     console.log("searchHistory not found")
+// }else{
+//     console.log("searchHistory loaded into searchHistoryArr");
+//     renderSearchHistory();
+// }
+  
+  
+//   localStorage.setItem("searchHistory", JSON.stringify(citiesArray));
+
+//   function renderSearchHistory(cityName) {
+//     searchHistoryEl.empty();
+//     var citiesArray = JSON.parse(localStorage.getItem("searchHistory"));
+//     for (let i = 0; i < citiesArray.length; i++) {
+//         // We put newListItem in loop because otherwise the text of the li element changes, rather than making a new element for each array index
+//         let newListItem = $("<li>").attr("class", "historyEntry");
+//         newListItem.text(citiesArray[i]);
+//         searchHistoryEl.prepend(newListItem);
+//     }
+// }
+
+  var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + APIKey +"&units=imperial";
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+    }).then(function(response) {
+      console.log(response);
+      cityNameEl.text(response.name);
+      var temp = response.main.temp;
+      var humidity = response.main.humidity;
+      var windSpeed = response.wind.speed;
+      var lat = response.coord.lat;
+      var lon = response.coord.lon;
+      var weatherIcon = response.weather[0].icon;
+
+      var renderedWeatherIcon = "https:///openweathermap.org/img/w/" + weatherIcon + ".png";
+
+      currentDateEl.text("(" + dateDisplay + ")");
+      weatherIconEl.attr("src", renderedWeatherIcon);
+      temperatureEl.text("Temperature: " + temp +" °F");
+      humidityEl.text("Humidity: " + humidity + "%");
+      windSpeedEl.text("Wind Speed: " + windSpeed + "MPH");
+
+      /**********************************************************/
+
+    
+      var queryURL = "http://api.openweathermap.org/data/2.5/uvi?appid=" + APIKey + "&lat=" + lat + "&lon=" + lon;
+      $.ajax({
+        url: queryURL,
+        method: "GET"
+       }).then(function(response) {
+        var uvIndex = response.value;
+        uvIndexEl.text("UV Index: " + uvIndex);
+
+        if ( uvIndex >=8 ) {
+          uvIndexEl.html('UV Index: '+'<span style= "background-color: red; color: white; border-radius: 5px; padding: 5px;"> '+uvIndex+' </span>');
+        }
+
+        if ( uvIndex >=6 && uvIndex <=7 ) {
+          uvIndexEl.html('UV Index: '+'<span style= "background-color: orange; color: white; border-radius: 5px;"> '+uvIndex+' </span>');
+        }
+
+        if ( uvIndex >=3 && uvIndex <=5 ) {
+          uvIndexEl.html('UV Index: '+'<span style= "background-color: yellow; color: white; border-radius: 5px;"> '+uvIndex+' </span>');
+        }
+
+        if ( uvIndex >=0 && uvIndex <=2 ) {
+          uvIndexEl.html('UV Index: '+'<span style= "background-color: green; color: white; border-radius: 5px;"> '+uvIndex+' </span>');
+        }
+
+      });
+
+    });
+
+        cardRow.empty();
+        var queryUrl = "http://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&appid=" + APIKey + "&units=imperial";
+        $.ajax({
+            url: queryUrl,
+            method: "GET"
+        }).then(function(response) {
+          console.log(response)
+
+          for (var i = 0; i < 5; i++) {
+            var date = response.list[i].dt_txt.substring(0, 10);
+            var icon = response.list[i].weather[0].icon;
+            var temp = response.list[i].main.temp;
+            var humidity = response.list[i].main.humidity;
+
+            var fiveDivs = $("<div>").attr("class", "five-day-card");
+            var cardDate = $("<h3>").attr("class", "card-text");
+            var cardIcon = $("<img>").attr("class", "weatherIcon");
+            var cardTemp = $("<p>").attr("class", "card-text");
+            var cardHumidity = $("<p>").attr("class", "card-text");
+            var weatherIcon = "https:///openweathermap.org/img/w/" + icon + ".png";
+        
+            cardRow.append(fiveDivs);
+            cardDate.text(date);
+            cardIcon.attr("src", weatherIcon);
+            cardTemp.text("Temp: " + temp + " °F");
+            cardHumidity.text("Humidity: " +  humidity + "%");
+            fiveDivs.append(cardDate, cardIcon, cardTemp, cardHumidity);
+          }
+
+        });
+
+        
+
+       
+
+
+
+
+
+
+
+
+  
+});
+
+
+
+  
